6f6de08f3329a8238c11c91304520d5be6d8122a
DOATOONs <jtcotoons@gmail.com>
2025-04-07 19:32:26 +0100

    Initial commit: Add entire DOATOONs website

---
+++ search-data.js
@@ -0,0 +1,44 @@
+const pages = [
+    {
+        title: 'Home',
+        url: 'home.html',
+        keywords: ['home', 'welcome', 'doatoons'],
+        text: 'Welcome to our animation website! Explore a world of imagination and storytelling through our collection of animated content.',
+        image: 'Logos/Doatoons.png'
+    },
+    {
+        title: 'Animations',
+        url: 'animations.html',
+        keywords: ['animations', '2d', '3d', 'hand drawn', 'sprite', 'stickman'],
+        text: 'Explore our collection of 2D and 3D animations, featuring hand-drawn, sprite sheets, and stickman styles.',
+        image: 'Images/Iron man.png'
+    },
+    {
+        title: 'Designs',
+        url: 'designs.html',
+        keywords: ['designs', 'digital renderings', 'typeface', 'custom'],
+        text: 'Our unique and innovative collection features modern and classic styles tailored to your imaginations.',
+        image: 'Images/Eye.png'
+    },
+    {
+        title: 'Digital Renderings',
+        url: 'digital-renderings.html',
+        keywords: ['designs', 'digital renderings', 'typeface', 'custom'],
+        text: 'Our unique and innovative collection features modern and classic styles tailored to your imaginations.',
+        image: 'Images/Eye.png'
+    },
+    {
+        title: 'Visual Effects',
+        url: 'visual-effects.html',
+        keywords: ['visual effects', 'goku', 'sonic', 'ssj'],
+        text: 'Check out our stunning visual effects gallery featuring characters like Goku and Sonic.',
+        image: 'Images/Jace Norman.png'
+    },
+    {
+        title: 'About Us',
+        url: 'about-us.html',
+        keywords: ['about', 'doatoons', 'teenagers', 'animation studio'],
+        text: 'DOATOONs is an animation studio dedicated to telling powerful stories that transcend age, culture, and language.',
+        image: 'Logos/my logo.svg'
+    }
+];
